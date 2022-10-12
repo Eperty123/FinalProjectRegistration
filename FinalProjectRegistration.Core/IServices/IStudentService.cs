67e5f9abd478bba91db272780cd633a22bdc606e
Onii-chan <failer007@live.dk>
2022-10-12 11:26:19 +0200

    Created IStudentService and Student model

---
+++ FinalProjectRegistration.Core/IServices/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using FinalProjectRegistration.Core.Models;
+
+namespace FinalProjectRegistration.Core.IServices
+{
+    public interface IStudentRepository
+    {
+        IEnumerable<Student> GetAll();
+        Student GetById(int id);
+        void Add(Student s);
+        void Update(Student s);
+        void Remove(Student s);
+    }
+}
